18ece7944af3744d4dbac3166f6023a9f525638f
Eugene <extrajack16@gmail.com>
2015-06-07 17:55:19 +0300

    xing

--- protected/components/views/uloginWidget.php
+++ protected/components/views/uloginWidget.php
@@ -7,8 +7,8 @@
 
 <?php if (Yii::app()->user->isGuest): ?>
     <div id="uLoginCustomCallback" class="social-links" data-ulogin="display=<?php echo $display ?>;fields=<?php echo $fields ?>;lang=<?php echo $lang ?>providers=<?php echo $providers ?>;hidden=<?php echo $hidden ?>;redirect_uri=<?php echo urlencode($redirect) ?>; lang=<?php echo $lang?>">
-        <div class="fa fa-facebook" data-uloginbutton = "facebook"></div>
-        <div class="fa fa-linkedin" data-uloginbutton = "linkedin"></div>
-        <div class="fa fa-xing"></div>
+        <a class="fa fa-facebook" data-uloginbutton = "facebook"></a>
+        <a class="fa fa-linkedin" data-uloginbutton = "linkedin"></a>
+        <a class="fa fa-xing"></a>
     </div>
 <?php endif ?>
