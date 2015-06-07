c4ff9446f88f58fe2f2869c95b64e202381e2298
Eugene <extrajack16@gmail.com>
2015-06-07 16:04:58 +0300

    xing

--- protected/components/views/uloginWidget.php
+++ protected/components/views/uloginWidget.php
@@ -5,16 +5,3 @@
         <div class="fa fa-xing"></div>
     </div>
 <?php endif ?>
-
-<script type="xing/login">
-    {
-      "consumer_key": "8408e4360eaec7710b97"
-    }
-    </script>
-
-<script>(function(d) {
-        var js, id='lwx';
-        if (d.getElementById(id)) return;
-        js = d.createElement('script'); js.id = id; js.src = "https://www.xing-share.com/plugins/login.js";
-        d.getElementsByTagName('head')[0].appendChild(js)
-    }(document));</script>
