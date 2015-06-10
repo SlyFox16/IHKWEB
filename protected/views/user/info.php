19285f8c2a5ce0084a197f87648c0319fb2dfc52
jenya <jenya@idol-it.com>
2015-06-10 15:55:16 +0300

    register

--- protected/views/user/info.php
+++ protected/views/user/info.php
@@ -104,6 +104,18 @@
                             <a href="<?php echo $user->xing_url; ?>"><?php echo $user->fullname;?></a>
                         </li>
                     <?php } ?>
+                    <?php if(!empty($user->facebook_url)) { ?>
+                        <li class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
+                            <i class="fa fa-xing"></i>
+                            <a href="<?php echo $user->facebook_url; ?>">Facebook</a>
+                        </li>
+                    <?php } ?>
+                    <?php if(!empty($user->twitter_url)) { ?>
+                        <li class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.9s">
+                            <i class="fa fa-xing"></i>
+                            <a href="<?php echo $user->twitter_url; ?>">Twitter</a>
+                        </li>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
