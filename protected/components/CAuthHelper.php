934c8e6069cf3dac30f5586a61de5888f50e4d88
jenya <jenya@idol-it.com>
2015-06-10 10:13:46 +0300

    self profile

--- protected/components/CAuthHelper.php
+++ protected/components/CAuthHelper.php
@@ -12,6 +12,10 @@ class CAuthHelper {
         $user = User::model()->is_active()->findByPk($id);
         if($user && !$user->is_staff && $user->expert_confirm)
             return true;
+
+        if($id == Yii::app()->user->id)
+            return true;
+
         if($user->is_staff)
             return true;
 
