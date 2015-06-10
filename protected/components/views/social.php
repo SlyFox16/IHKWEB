fc931e48bc52346578d8b88e26d8bbc2f42b083b
jenya <jenya@idol-it.com>
2015-06-10 16:22:40 +0300

    feedback

--- protected/components/views/social.php
+++ protected/components/views/social.php
@@ -6,7 +6,7 @@
         <?php echo CHtml::link('&nbsp;', $twitter_url, array('class' => 'fa fa-twitter', 'target' => "_blank")); ?>
     <?php } ?>
     <?php if($linkedin_url) { ?>
-        <?php echo CHtml::mailto('&nbsp;', $linkedin_url, array('class' => 'fa fa-linkedin')); ?>
+        <?php echo CHtml::link('&nbsp;', $linkedin_url, array('class' => 'fa fa-linkedin')); ?>
     <?php } ?>
     <?php if($gp_url) { ?>
         <?php echo CHtml::link('&nbsp;', $gp_url, array('class' => 'fa fa-google-plus')); ?>
