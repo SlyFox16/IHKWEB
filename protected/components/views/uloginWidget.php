4889db53f71911aeabb2e66b466b55915a61310b
Eugene <extrajack16@gmail.com>
2015-06-15 00:29:19 +0300

    xing login

--- protected/components/views/uloginWidget.php
+++ protected/components/views/uloginWidget.php
@@ -2,6 +2,7 @@
     <div id="uLoginCustomCallback" class="social-links" data-ulogin="display=<?php echo $display ?>;fields=<?php echo $fields ?>;lang=<?php echo $lang ?>providers=<?php echo $providers ?>;hidden=<?php echo $hidden ?>;redirect_uri=<?php echo urlencode($redirect) ?>; lang=<?php echo $lang?>">
         <div class="fa fa-facebook" data-uloginbutton = "facebook"></div>
         <div class="fa fa-linkedin" data-uloginbutton = "linkedin"></div>
-        <div onclick="location.href='<?php echo Yii::app()->createUrl('site/xing'); ?>'" class="fa fa-xing"></div>
+        <!--<div onclick="location.href='<?php /*echo Yii::app()->createUrl('site/xing'); */?>'" class="fa fa-xing"></div>-->
+        <div class="fa fa-xing"></div>
     </div>
 <?php endif ?>
