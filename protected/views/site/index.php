deffa378c34af0e97a57208f4ef154fd6623fc84
Eugene <extrajack16@gmail.com>
2015-06-17 00:27:12 +0300

    level

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -22,7 +22,7 @@
                         <?php } ?>
                     <?php } ?>
                 </ul>
-                <span class="expert-level">Level 1</span>
+                <span class="expert-level">Level <?php echo $user->level; ?></span>
             </div>
         </div>
     <?php } ?>
