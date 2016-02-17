2257bc77eab8c789eea9c0fe20fb93e1c5bafe53
Jenia Stratila <Jenia@MacBook-Air.local>
2016-02-18 00:04:29 +0200

    User Relation

--- protected/widgets/views/user_relation.php
+++ protected/widgets/views/user_relation.php
@@ -12,44 +12,48 @@
             <?php } ?>
         <?php } ?>
     </ul>
-</li>
-
-<div class="row" style="margin-top:20px">
-    <div class="col-sm-10">
-        <div class="form-group">
-            <?php $this->widget(
-                'booster.widgets.TbSelect2',
-                [
-                    'name' => 'ProductSelect',
-                    'asDropDownList' => false,
-                    'options' => [
-                        'minimumInputLength' => 2,
-                        'placeholder' => 'Select user',
-                        'width' => '100%',
-                        'allowClear' => true,
-                        'ajax' => [
-                            'url' => Yii::app()->controller->createUrl('/site/suggest'),
-                            'dataType' => 'json',
-                            'data' => 'js:function(term, page) { return {term: term }; }',
-                            'results' => 'js:function(data) { return {results: data}; }',
-                        ],
-                        'formatResult' => 'js:productFormatResult',
-                        'formatSelection' => 'js:productFormatSelection',
-                    ],
-                    'htmlOptions' => [
-                        'id' => 'user-select',
-                        'class' => 'form-control'
-                    ],
-                ]
-            );?>
+    <div class="field-content">
+        <div>
+            Related User
+        </div>
+        <div>
+            <ul class="add-input">
+                <li>
+                    <?php $this->widget(
+                        'booster.widgets.TbSelect2',
+                        [
+                            'name' => 'ProductSelect',
+                            'asDropDownList' => false,
+                            'options' => [
+                                'minimumInputLength' => 2,
+                                'placeholder' => 'Select user',
+                                'width' => '100%',
+                                'allowClear' => true,
+                                'ajax' => [
+                                    'url' => Yii::app()->controller->createUrl('/site/suggest'),
+                                    'dataType' => 'json',
+                                    'data' => 'js:function(term, page) { return {term: term }; }',
+                                    'results' => 'js:function(data) { return {results: data}; }',
+                                ],
+                                'formatResult' => 'js:productFormatResult',
+                                'formatSelection' => 'js:productFormatSelection',
+                            ],
+                            'htmlOptions' => [
+                                'id' => 'user-select',
+                                'class' => 'form-control'
+                            ],
+                        ]
+                    );?>
+                </li>
+                <li>
+                    <a class="fa fa-plus" id="add-user-to-user"></a>
+                </li>
+            </ul>
         </div>
     </div>
-    <div class="col-sm-2">
-        <a class="btn btn-primary btn btn-default btn-block" href="#" id="add-user-to-user">
-            <?= Yii::t('base','Add'); ?>
-        </a>
-    </div>
-</div>
+</li>
+
+
 
 <script type="text/javascript">
     $('#add-user-to-user').click(function (e) {
