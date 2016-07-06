0e34b6fa8e8e63475d9681ae9bb9bea06a85aefc
Jenya <jenya@idol-it.com>
2016-07-06 11:11:58 +0300

    xing

--- protected/models/IhkFacebookService.php
+++ protected/models/IhkFacebookService.php
@@ -13,16 +13,10 @@ class IhkFacebookService extends FacebookOAuthService {
 	protected function fetchAttributes() {
 		$this->attributes = (array)$this->makeSignedRequest('https://graph.facebook.com/v2.5/me', array(
 			'query' => array(
-				'fields' => join(',', array(
-					'name',
-                    'link',
-					'email',
-					'first_name',
-					'last_name',
-				))
+
 			)
 		));
-
+print_r($this->attributes); die();
         $this->attributes['network'] = 'facebook';
 	}
 }
