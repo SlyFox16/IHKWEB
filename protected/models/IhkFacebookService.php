309bf5508c29a5748abd9c2b3b03f482834cbd90
Jenya <jenya@idol-it.com>
2016-07-06 11:40:37 +0300

    xing

--- protected/models/IhkFacebookService.php
+++ protected/models/IhkFacebookService.php
@@ -23,7 +23,8 @@ class IhkFacebookService extends FacebookOAuthService {
 				))
 			)
 		));
-print_r($this->attributes); die();
+print_r($this->attributes['picture']['data']['url']); die();
         $this->attributes['network'] = 'facebook';
+        $this->attributes['avatar'] = $this->attributes['picture']['data']['url'];
 	}
 }
