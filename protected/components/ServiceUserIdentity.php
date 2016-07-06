5400a28b3b766944815152a48694d3e58f2f1e71
Jenya <jenya@idol-it.com>
2016-07-06 10:53:38 +0300

    xing

--- protected/components/ServiceUserIdentity.php
+++ protected/components/ServiceUserIdentity.php
@@ -35,10 +35,10 @@ class ServiceUserIdentity implements IUserIdentity
             $user->name = $uloginModel->first_name;
             $user->surname = $uloginModel->last_name;
 
-            $user->companyname = $uloginModel->company;
-            $user->description = $uloginModel->description;
-            $user->position = $uloginModel->position;
-            $user->avatar = $uloginModel->avatar;
+            $user->companyname = @$uloginModel->company;
+            $user->description = @$uloginModel->description;
+            $user->position = @$uloginModel->position;
+            $user->avatar = @$uloginModel->avatar;
 
             $user->is_active = 1;
             $user->is_staff = 0;
