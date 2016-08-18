f2a9c781689df96c6c1d84f2d19c82a09dcca44e
Jenya <jenya@idol-it.com>
2016-08-18 11:59:20 +0300

    new files fron crowdexperts

--- protected/components/views/report_pop.php
+++ protected/components/views/report_pop.php
@@ -5,33 +5,33 @@
                 <button class="close" aria-hidden="true" data-dismiss="modal" type="button"></button>
                 <h5 class="text-left p-b-5"><span class="semi-bold">Report</span></h5>
             </div>
-            <div class="modal-body">
-                <?php $form = $this->beginWidget('CActiveForm', array(
-                    'id' => 'report-form',
-                    'action' => array('user/report'),
-                    'enableAjaxValidation' => true,
-                    'clientOptions' => array(
-                        'validateOnSubmit' => true,
-                        'validateOnChange' => true
-                    ),
-                )); ?>
-                <fieldset>
-                    <ul class="fields">
-                        <li>
-                            <div class="field-content">
-                                <div><?php echo $form->label($model, 'text'); ?></div>
-                                <div><?php echo $form->textArea($model, 'text'); ?></div>
-                            </div>
-                            <?php echo $form->error($model, 'text'); ?>
-                        </li>
-                    </ul>
-                    <?php echo $form->hiddenField($model, 'receiver'); ?>
-                </fieldset>
-            </div>
-            <div class="modal-footer">
-                <button type="submit" class="btn btn-primary btn-block m-t-5"><?php echo Yii::t("base", "Send report"); ?></button>
-                <?php $this->endWidget(); ?>
-            </div>
+            <?php $form = $this->beginWidget('CActiveForm', array(
+                'id' => 'report-form-my',
+                'action' => array('user/report'),
+                'enableAjaxValidation' => true,
+                'clientOptions' => array(
+                    'validateOnSubmit' => true,
+                    'validateOnChange' => true
+                ),
+            )); ?>
+                <div class="modal-body">
+                    <fieldset>
+                        <ul class="fields">
+                            <li>
+                                <div class="field-content">
+                                    <div><?php echo $form->label($model, 'text'); ?></div>
+                                    <div><?php echo $form->textArea($model, 'text'); ?></div>
+                                </div>
+                                <?php echo $form->error($model, 'text'); ?>
+                            </li>
+                        </ul>
+                        <?php echo $form->hiddenField($model, 'receiver'); ?>
+                    </fieldset>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary btn-block m-t-5"><?php echo Yii::t("base", "Send report"); ?></button>
+                </div>
+            <?php $this->endWidget(); ?>
         </div>
         <!-- /.modal-content -->
     </div>
