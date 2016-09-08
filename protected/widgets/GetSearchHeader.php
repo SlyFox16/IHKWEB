664a763b74fe73e93d157fbb77b1acdd80912f38
Eugene <extrajack16@gmail.com>
2016-09-08 09:23:51 +0300

    new translation

--- protected/widgets/GetSearchHeader.php
+++ protected/widgets/GetSearchHeader.php
@@ -11,12 +11,13 @@ class GetSearchHeader extends CWidget
         if ($this->model->name) $name = '<b>'.$this->model->name.'</b>';
         if ($this->model->surname) $surname = '<b>'.$this->model->surname.'</b>';
 
+        $serchQuery[] = trim($name.' '.$surname);
         if ($this->model->city_id) $serchQuery[] = '<b>'.User::getCityCountry($this->model->city_id, 'city').'</b>';
         if ($this->model->level >= 0 && $this->model->level != NULL) $serchQuery[] = Yii::t("base", 'level').': <b>'.$this->model->level.'</b>';
         if ($this->model->rating >= 0 && $this->model->rating != NULL) $serchQuery[] = Yii::t("base", 'rating').': <b>'.$this->model->rating.'</b>';
 
         foreach ($serchQuery as $key => $value)
-            trim($serchQuery[$key]);
+            $serchQuery[$key] = trim($value);
 
         $serchQuery = implode(', ',$serchQuery);
 
