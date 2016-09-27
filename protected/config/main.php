5595b379bc3f6b2f9ab222c6f9f9dd71ec22580d
Jenya <jenya@idol-it.com>
2016-09-27 13:17:17 +0300

    new getImagePath

--- protected/config/main.php
+++ protected/config/main.php
@@ -176,6 +176,7 @@ return CMap::mergeArray(
                     '<controller:\w+>/<id:\d+>' => '<controller>/view',
                     '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                     '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                    '<controller:\w+>/<id:\w+>' => '<controller>/view',
                     '<module:\w+>/<controller:\w+>/<action:\w+>/<id:\d+>' => '<module>/<controller>/<action>',
                     '<module:\w+>/<controller:\w+>/<action:\w+>/<param:\w+>' => '<module>/<controller>/<action>',
                     '<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>'
