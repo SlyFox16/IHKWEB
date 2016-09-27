4bf8433c59cede7b08a8bca31444515f76e46b5b
Jenya <jenya@idol-it.com>
2016-09-27 12:48:16 +0300

    new getImagePath

--- protected/config/main.php
+++ protected/config/main.php
@@ -172,6 +172,7 @@ return CMap::mergeArray(
                     'ratingtop' => 'site/ratingList',
                     'user/recover/<pass:\w+>' => 'user/recover',
                     '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                    '<controller:\w+>/<id:\w+>' => '<controller>/view',
                     '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                     '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
                     '<module:\w+>/<controller:\w+>/<action:\w+>/<id:\d+>' => '<module>/<controller>/<action>',
