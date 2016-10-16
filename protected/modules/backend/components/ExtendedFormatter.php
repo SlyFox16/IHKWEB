89634400ddd005f17b768b9bd41405903ba6b966
Eugene <extrajack16@gmail.com>
2016-10-17 00:33:32 +0300

    new version

--- protected/modules/backend/components/ExtendedFormatter.php
+++ protected/modules/backend/components/ExtendedFormatter.php
@@ -26,7 +26,7 @@ class ExtendedFormatter extends CFormatter
 
     public function formatImage($value)
     {
-        return CHtml::image(YHelper::getImagePath($value, 100), 'user', array('width' => '100px'));
+        return CHtml::image(YHelper::getImagePath($value, 100), 'user', array('width' => '150px'));
     }
 
     public function formatLogo($value)
