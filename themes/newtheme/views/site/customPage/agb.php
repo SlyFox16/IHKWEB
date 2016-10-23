0f277eb402c036de4a45e66c4f1202eabfea3259
Jenia Stratila <Jenia@MacBook-Air.local>
2016-10-23 14:58:24 +0300

    AGB

--- themes/newtheme/views/site/customPage/agb.php
+++ themes/newtheme/views/site/customPage/agb.php
@@ -1,1 +1,34 @@
-111
+<div class="row">
+	<div class="small-12 columns">
+		<h1>Allgemeine Geschäftsbedingungen und Kundeninformationen</h1>
+	</div>
+	<div class="small-12 columns">
+		<ul class="accordion" data-accordion data-allow-all-closed="true">
+		    <li class="accordion-item" data-accordion-item>
+		        <a href="#" class="accordion-title">I Einleitende Bestimmungen</a>
+		        <div class="accordion-content" data-tab-content>
+		            <p>
+		                1.1. Diese Allgemeinen Geschäfts- und Nutzungsbedingungen („AGB“) gelten für alle Verträge, die zwischen dem DCV e.V. & German Crowdfunding Network, Karl-Liebknecht-Straße 34, 10178 Berlin, vertreten durch den jeweiligen vertretungsberechtigten Vorstand (nachfolgend Anbieterin) und einem Kunden (nachfolgend „Berater“) über die von dem DCV e.V. & German Crowdfunding Network angebotenen Waren und/ oder Leistungen persönlich, per E-Mail, per Telefon oder direkt über die Website abgeschlossen werden, ganz gleich, über welches Endgerät (z.B. PC, Mobiltelefon) und über welche der dazugehörigen Domains der Berater die Dienste der Anbieterin aufruft.
+						1.2. Vorformulierte Bedingungen des Beraters, welche von den vorliegenden AGB abweichen, werden selbst bei Kenntnis nicht Vertragsbestandteil, es sei denn, die Anbieterin stimmt ihrer Geltung ausdrücklich schriftlich zu.
+						1.3. Verbraucher ist hierbei jede natürliche Person, die ein Rechtsgeschäft zu Zwecken abschließt, die überwiegend weder ihrer gewerblichen noch ihrer selbständigen beruflichen Tätigkeit zugerechnet werden können. Unternehmer ist eine natürliche oder juristische Person oder eine rechtsfähige Personengesellschaft, die bei Abschluss eines Rechtsgeschäfts in Ausübung ihrer gewerblichen oder selbständigen beruflichen Tätigkeit handelt.
+						1.4. Vertragssprache ist Deutsch.
+		            </p>
+		        </div>
+		    </li>
+		    <li class="accordion-item" data-accordion-item>
+		        <a href="#" class="accordion-title">II Vertragsgegenstand, Beraterprofil</a>
+		        <div class="accordion-content" data-tab-content>
+		            <p>
+		                2.1. Gegenstand des Vertrages ist die Präsentation des Beraters mit einem kostenfreien Profileintrag (Beraterprofil) sowie die Erbringung von Dienstleistungen. Die wesentlichen Merkmale der Waren bzw. Inhalt und Umfang der gebuchten Leistungen ergeben sich aus den jeweiligen Leistungsbeschreibungen auf der Website. Das Profil wird über die Suchergebnisse verlinkt. Die Anbieterin behält sich vor, zusätzliche – auch kostenpflichtige – Angebote einzuführen, für die separate AGB Anwendung finden können.
+		                2.2. Bei Registrierung stellt der Berater mindestens Grunddaten und Materialien zur Verfügung, die zur Veröffentlichung des Beraterprofils geeignet sind. Der Berater ist verpflichtet, sich mit seinem natürlichen Namen anzumelden. Pseudonyme sind nicht zugelassen.
+		                2.3. Der Berater gestattet der Anbieterin die Verwendung von Daten, insbesondere von Text- und Bildmaterial, welches vom Berater kostenfrei zur Verfügung gestellt wird.
+		                2.4. Der Berater erhält Zugang zu seinem Beraterprofil über ein persönliches Login. Ein Anspruch auf Veröffentlichung bestimmter Inhalte besteht nicht.
+		                2.5. Der Berater wird unter Nennung seines Namens, seiner Unternehmensbezeichnung/Firma und Kontaktdaten im Beraternetzwerk, seines Lichtbilds und/oder Firmenlogos genannt.
+		                2.6. Der Berater wird keine Daten, Unterlagen oder Informationen an die Anbieterin übermitteln, deren Inhalt die Rechte Dritter (insbesondere Urheberrechte, Namensrechte, Markenrechte) verletzen oder gegen bestehende Gesetze verstoßen. Der Berater stellt die Anbieterin ausdrücklich von sämtlichen in diesem Zusammenhang geltend gemachten Ansprüchen Dritter frei. Das betrifft auch die Kosten der in diesem Zusammenhang erforderlichen rechtlichen Vertretung.
+		                2.7. Die Anbieterin überprüft die vom Berater übermittelten Daten, Informationen und Unterlagen hierbei nicht auf inhaltliche Richtigkeit, Vollständigkeit und Rechtskonformität.
+		            </p>
+		        </div>
+		    </li>
+		</ul>
+	</div>
+</div>
