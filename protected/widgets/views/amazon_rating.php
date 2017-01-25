ac96fb3aa722ff7ce34464929e7a01eeae529800
Jenia Stratila <Jenia@MacBook-Air.local>
2017-01-25 19:19:01 +0200

    Rating Summary

--- protected/widgets/views/amazon_rating.php
+++ protected/widgets/views/amazon_rating.php
@@ -1,18 +1,18 @@
-<div class="poll-results">
-    <table cellspacing="0" cellpadding="0">
-        <tbody>
-            <?php foreach ($ratings as $num => $count) { ?>
-                <tr>
-                    <td colspan="2" class="page_poll_text"><?php echo Yii::t('base', '{n} star|{n} stars', $num); ?></td>
-                    <td>
-                        <?php echo CHtml::tag('div', array('id' => 'stars_' . $num, 'class' => 'rating'));
-                        Yii::app()->clientScript->registerScript("stars_$num", "
-                        $('#stars_" . $num . "').raty({readOnly: true, score: " . $num . "});
-                        ", CClientScript::POS_READY); ?>
-                    </td>
-                    <td class="page_poll_row_percent ta_r"><nobr><b><?php echo $count; ?></b></nobr></td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+<div class="summary">
+    <ul>
+        <?php foreach ($ratings as $num => $count) { ?>
+        <li>
+            <div class="summary-counter"><?php echo Yii::t('base', '{n} star|{n} stars', $num); ?></div>
+            <div class="summary-stars">
+                <?php echo CHtml::tag('div', array('id' => 'stars_' . $num, 'class' => 'rating'));
+                Yii::app()->clientScript->registerScript("stars_$num", "
+                $('#stars_" . $num . "').raty({readOnly: true, score: " . $num . "});
+                ", CClientScript::POS_READY); ?>
+            </div>
+            <div class="summary-ammount">
+                <?php echo $count; ?>
+            </div>
+        </li>
+        <?php } ?>
+    </ul>
 </div>
